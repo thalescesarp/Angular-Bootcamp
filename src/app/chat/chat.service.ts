f0376f65c73a5420e3a526fdb4177a724341132e
bootcamp <bootcamp@suportes-Mac-mini.local>
2017-11-25 14:23:30 -0200

    connected to socket.io server

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Injectable()
 export class ChatService {
 
   private user: string = '';
   private serverURL: string = 'http://bootcamp.us-east-1.elasticbeanstalk.com/';
+  private server: any;
 
   get userName(): string {
     return this.user;
@@ -19,6 +21,8 @@ export class ChatService {
     } else {
       this.user = storedName;
     }
+
+    this.server = io(this.serverURL);
   }
 
 }
